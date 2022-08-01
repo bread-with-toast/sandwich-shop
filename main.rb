6f6eb20eca15c8be643699486dbf003d2e97f715
Manar Ali <ahmadshehadeh78@gmail.com>
2022-08-01 16:30:10 +0100

    Added other sandwich + sleep

--- main.rb
+++ main.rb
@@ -19,13 +19,24 @@ class Sandwich
     end
 
     def log()
-        puts "\n\e[45mInformation About #{name}\e[0m"
-        puts "--------------------------------------\n"
-        puts "Is It Tasty: #{is_tasty}"
+        puts "\n--------------------------------------"
+        puts "\e[45m Information About #{name} \e[0m"
+        puts "--------------------------------------"
+        if is_tasty
+            puts "Is It Tasty? \e[42mYes\e[0m"
+        else
+            puts "Is It Tasty? \e[41mNo\e[0m"
+        end
+
         puts "Price: #{price}"
         puts "Available: #{available}\n"
+
+        sleep 4
     end
 end 
 
-mySandwich = Sandwich.new("The Big Pro Double Extra", true, 8.62, 32)
+mySandwich = Sandwich.new("The Big Pro Double Extra", false, 8.62, 32)
 mySandwich.log
+
+miniSandwich = Sandwich.new("The Small Lite Half Normal", true, 4.31, 16)
+miniSandwich.log
