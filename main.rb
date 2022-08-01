bb4e515fe19ca8c8c03e0613dcbb7a42bcfc7a52
Manar Ali <ahmadshehadeh78@gmail.com>
2022-08-01 16:21:46 +0100

    Added colours and name

--- main.rb
+++ main.rb
@@ -1,7 +1,8 @@
 class Sandwich
-    attr_accessor :is_tasty, :price, :available
+    attr_accessor :name, :is_tasty, :price, :available
 
-    def initialize(is_tasty, price, available)
+    def initialize(name, is_tasty, price, available)
+        @name = name
         @is_tasty = is_tasty
         @price = price
         @available = available
@@ -16,8 +17,15 @@ class Sandwich
         @available += amount
         puts "\e[42mAvailable Now: #{@available}\e[0m"
     end
+
+    def log()
+        puts "\n\e[45mInformation About #{name}\e[0m"
+        puts "--------------------------------------\n"
+        puts "Is It Tasty: #{is_tasty}"
+        puts "Price: #{price}"
+        puts "Available: #{available}\n"
+    end
 end 
 
-mySandwich = Sandwich.new(true, 8.62, 32)
-mySandwich.sell(3)
-mySandwich.make(2)
+mySandwich = Sandwich.new("The Big Pro Double Extra", true, 8.62, 32)
+mySandwich.log
